7038e123f50291b48bd5e71a88ae09994959beb4
rafay529 <113420528+rafay529@users.noreply.github.com>
2022-09-19 20:07:08 +0500

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddelware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -117,6 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = 'staticfiles/'
 MEDIA_URL = '/images/'
 
